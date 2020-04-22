6db56da788b5616f6c92c27371398b11d92d928a
sajid7053 <39021751+sajid7053@users.noreply.github.com>
2020-04-22 17:46:31 +0530

    Update MyModuleForm.php

--- src/Form/MyModuleForm.php
+++ src/Form/MyModuleForm.php
@@ -2,66 +2,60 @@
 
 namespace Drupal\my_module\Form;
 
-use Drupal\Core\Database\Database;
 use Drupal\Core\Form\Formbase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\Node;
-
+use Drupal\Core\Messenger\MessengerTrait;
 
 class MyModuleForm extends FormBase {
-  
-
   public function getFormId() {
     return 'my_module_new_form';
   }
     
-   public function buildForm(array $form, FormStateInterface $form_state) { 
-   $node_types = \Drupal\node\Entity\NodeType::loadMultiple();
-   $options = [];
-   foreach ($node_types as $node_type) {
-     $options[$node_type->id()] = $node_type->label();
-   }
-     $form['NODES'] = array(
-       '#title' => t('Enter the number'),
-       '#type' => 'number',
-       '#required' => TRUE,
-    );
-	$form['contenttypes'] = array(
+  public function buildForm(array $form, FormStateInterface $form_state) { 
+    $node_types = \Drupal\node\Entity\NodeType::loadMultiple();
+    $options = [];
+    foreach ($node_types as $node_type) {
+      $options[$node_type->id()] = $node_type->label();
+    }
+    $form['mynodes'] = [
+      '#title' => t('Enter the number'),
+      '#type' => 'number',
+      '#required' => TRUE,
+    ];
+	$form['contenttypes'] = [
 	  '#title' => t('Select any of the content types'),
 	  '#type' => 'select',
 	  '#options' => $options,
-	);
-    $form['submit'] = array(
+	  '#required' => TRUE,
+	];
+    $form['submit'] = [
       '#type' => 'submit',
       '#value' => t('Submit'),
-    );
-    $form['nid'] = array(
+    ];
+    $form['nid'] = [
       '#type' => 'hidden',
       '#value' => $nid,
-    );
+    ];
     return $form;
   }
  
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    drupal_set_message(t('This is working.'));
-	
-    $value = $form_state->getValue('NODES');
+    $value = $form_state->getValue('mynodes');
 	$x=0;
 	while($x<$value){
-	  $node = Node::create(array(
+	  $node = Node::create([
               'type' => 'Article',
               'title' => 'My Node',
               'langcode' => 'en',
               'uid' => '1',
               'status' => 1,
               'field_fields' => array(),
-            ));
+            ]);
 			$node->save();
 	  $x++; 
-            
-
-	
     }
+	$this->messenger()->addMessage('Success');
   }
 }
    
